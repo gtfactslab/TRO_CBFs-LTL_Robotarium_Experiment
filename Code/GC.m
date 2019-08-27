298cef4e5709b39981b8702eff395dcd007e9bca
Mohit Srinivasan <mohit.srinivasan@gmail.com>
2019-08-27 13:38:51 -0400

    Commented code

--- Code/GC.m
+++ Code/GC.m
@@ -6,6 +6,7 @@ function [hgx1, hgx2, hg3, hgoal31, hgoal32, dconn, hgo1, hgo2, y] = GC(X)
     
     P2 = [1/(0.4)^2 0; 0 1/(0.2)^2];
     P3 = [1/(0.3)^2 0; 0 1/(0.35)^2];
+    Pobs = [1/(0.3)^2 0; 0 1/(0.35)^2];
     
     c3 = 0.9; c4 = 0.7; c5 = 0.8; c6 = -0.7; c7 = -1; c8 = 0.4; c9 = 0.3; c10 = 0;
     
@@ -19,6 +20,7 @@ function [hgx1, hgx2, hg3, hgoal31, hgoal32, dconn, hgo1, hgo2, y] = GC(X)
     P_3 = [zero zero zero; zero P2 zero; zero zero zero];
     P_4 = [P3 zero zero; zero zero zero; zero zero zero];
     P_5 = [zero zero zero; zero P3 zero; zero zero zero];
+    P_obs = [zero zero zero; zero zero zero; zero zero Pobs];
 
     norm21 = norm(X(1:2) - X(3:4));
     dconn = (X(3) + 0.8)^2 + 0.2;
@@ -27,6 +29,7 @@ function [hgx1, hgx2, hg3, hgoal31, hgoal32, dconn, hgo1, hgo2, y] = GC(X)
     hgx2 = 1 - (X - [0; 0; C3g2; 0; 0])'*P_3*(X - [0; 0; C3g2; 0; 0]);           % Goal B for Robot 2 
     hgo1 = (X - [Cobs; 0; 0; 0; 0])'*P_4*(X - [Cobs; 0; 0; 0; 0]) - 1;
     hgo2 = (X - [0; 0; Cobs; 0; 0])'*P_5*(X - [0; 0; Cobs; 0; 0]) - 1;
+    hgo3 = (X - [0; 0; 0; 0; Cobs])'*P_3*(X - [0; 0; 0; 0; Cobs]) - 1;       %% Obstacle avoidance for robot 3
       
     hgoal31 = 1 - (X - [C2; 0; 0; 0; 0])'*P_2*(X - [C2; 0; 0; 0; 0]);            % Goal C for Robot 1
     hgoal32 = 1 - (X - [0; 0; C2; 0; 0])'*P_3*(X - [0; 0; C2; 0; 0]);            % Goal C for Robot 2
@@ -38,11 +41,13 @@ function [hgx1, hgx2, hg3, hgoal31, hgoal32, dconn, hgo1, hgo2, y] = GC(X)
     A2 = 2*(X - [C2; 0; 0; 0; 0])'*P_2 + 2*(X - [0 ; 0; C2; 0; 0])'*P_3;
     A4 = -2*(X - [Cobs; 0; 0; 0; 0])'*P_4;
     A5 = -2*(X - [0; 0; Cobs; 0; 0])'*P_5;
+    A6 = -2*(X - [0; 0; 0; 0; Cobs])'*P_obs;
     
     B2 = gamma*sign(min(hgoal31, hgoal32));
     B3 = gamma*hg3;
     B4 = gamma*hgo1^7;
     B5 = gamma*hgo2^7;
+    B6 = gamma*hgo3^3;
     
     a = [A2; A3; A4; A5];
     b = [B2; B3; B4; B5];
--- Code/ReachB.m
+++ Code/ReachB.m
@@ -17,6 +17,11 @@ function [hg3, y] = ReachB(X)
     P_2 = [zero zero zero; zero Pobs zero; zero zero zero];
     P_3 = [zero zero zero; zero zero zero; zero zero Pobs];
     P_4 = [zero zero zero; zero zero zero; zero zero Pshape];
+    
+    norm21 = norm(X(1:2) - X(3:4));
+    dconn = (X(3) + 0.8)^2 + 0.2;
+
+    h_conn = dconn^2 - norm21^2;
 
     hgo1 = (X - [Cobs; 0; 0; 0; 0])'*P_1*(X - [Cobs; 0; 0; 0; 0]) - 1;       %% Obstacle avoidance for robot 1
     hgo2 = (X - [0; 0; Cobs; 0; 0])'*P_2*(X - [0; 0; Cobs; 0; 0]) - 1;       %% Obstacle avoidance for robot 2
@@ -28,14 +33,17 @@ function [hg3, y] = ReachB(X)
     A2 = -2*(X - [Cobs; 0; 0; 0; 0])'*P_1;
     A3 = -2*(X - [0; 0; Cobs; 0; 0])'*P_2;
     A4 = -2*(X - [0; 0; 0; 0; Cobs])'*P_3;
+    A5 = - [0, 0, (2*dconn)*(2*(X(3) + 0.8)^1), 0] - [2*(X(3) - X(1)), 2*(X(4) - X(2)), -2*(X(3) - X(1)), - 2*(X(4) - X(2))];
+    A5 = [A5, 0, 0];
     
     B1 = gamma*sign(hg3)*abs(hg3)^(0.5);
     B2 = gamma*hgo1^7;
     B3 = gamma*hgo2^3;
     B4 = gamma*hgo3^3;
+    B5 = gamma*h_conn;
     
-    a = [A1; A2; A3; A4];
-    b = [B1; B2; B3; B4];
+    a = [A1; A2; A3; A4; A5];
+    b = [B1; B2; B3; B4; B5];
     
     opts = optimoptions(@quadprog, 'Display', 'off');
     y = quadprog(H, [], a, b, [], [], [], [], [], opts);  
